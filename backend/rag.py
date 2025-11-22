6c87b0b230f0ddc33030db968474c7b18f16634e
Yushan Hettiarachchi <130626574+Dismi343@users.noreply.github.com>
2025-11-22 23:31:22 +0530

    mongodb setuped for enter vector data

--- backend/rag.py
+++ backend/rag.py
@@ -1,5 +1,7 @@
-from rag_retrieval import retrieve_relevant_chunks
+#from rag_retrieval import retrieve_relevant_chunks #chromadb retrieval
 from llama_openrouter import answer_query_with_openrouter
+
+from Mongo_db.rag_retrieval_mongo import retrieve_relevant_chunks  #mongo retrieval
 
 def answer_query(query: str) -> str:
    
